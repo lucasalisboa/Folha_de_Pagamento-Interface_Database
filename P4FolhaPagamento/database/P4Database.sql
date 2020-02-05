28dcfa6d9348230efba13154b88ecbfc9eae40aa
João Victor Ribeiro Ferro <39158742+joaovictorferro@users.noreply.github.com>
2020-02-04 23:45:42 -0300

    att

--- P4FolhaPagamento/database/P4Database.sql
+++ P4FolhaPagamento/database/P4Database.sql
@@ -1,48 +1,63 @@
-create database P4Database;
+#create database P4Database;
 use P4Database;
 
 create table usuarios(
-    id_usuario int primary key auto_increment, 
-    usuario varchar(15) not null unique, 
-    senha varchar(15) not null,
+	id_usuario int primary key auto_increment, 
+	usuario varchar(15) not null unique, 
+	senha varchar(15) not null,
     tipo_usuario varchar(15) not null
 );
 
-create table empregados(
-    id_empregado int primary key auto_increment, 
-    nome varchar(50) not null, 
-    endereco varchar (50) not null, 
-    metodo_pagamento varchar(50) not null, 
-    tipo_funcionario varchar(50) not null, 
-    pertence_sindicato varchar(50) not null,
-    data_pagamento varchar(50) not null
-);
-
-create table horista(
-    id_horista int key primary key auto_increment, 
-    hora_salario decimal (10, 2) not null, 
-    hora decimal(10, 2), 
-    id_empregado int not null unique, 
-    foreign key (id_empregado) references empregados(id_empregado)
-);
-
-create table assalariado(
-    id_assalariado int key primary key auto_increment, 
-    salario decimal(10, 2) not null, 
-    id_empregado int not null unique, 
-    foreign key (id_empregado) references empregados(id_empregado)
-);
-
-create table comissionado(
-    id_comissionado int key primary key auto_increment, 
-    salario decimal(10, 2) not null, 
-    taxa_venda decimal(10, 2) not null, 
-    total_vendas decimal(10, 2) not null, 
-    id_empregado int not null unique, 
-    foreign key (id_empregado) references empregados(id_empregado)
-);
+describe usuarios;
+select * from usuarios;
 
 insert into usuarios(usuario, senha, tipo_usuario) values ("admin", "admin", "admin");
 insert into usuarios(usuario, senha, tipo_usuario) values ("", "", "admin");
 insert into usuarios(usuario, senha, tipo_usuario) values ("jeremias", "123", "user");
-insert into empregados (nome, endereco, metodo_pagamento, tipo_funcionario, pertence_sindicato, data_pagamento) values ("Vitor", "Avenida Brasil, 150, Maceió, Alagoas, Brasil", "Banco", "assalariado", "Sindicato", "1");
+
+create table empregados(
+	id_empregado int primary key auto_increment, 
+	nome varchar(50) not null, 
+	endereco varchar (50) not null, 
+	metodo_pagamento varchar(50) not null, 
+	tipo_funcionario varchar(50) not null, 
+	pertence_sindicato varchar(50) not null,
+    data_empregado varchar(50) not null
+);
+DELETE FROM empregados WHERE id_empregado = 22;
+select max(id_empregado) from empregados;
+select * from empregados;
+describe empregados;
+
+update horista set hora=8 where id_empregado=15;
+select * from horista;
+describe horista;
+create table horista(
+	id_horista int key primary key auto_increment, 
+	hora_salario decimal (10, 2) not null, 
+	hora varchar(50), 
+	id_empregado int not null unique, 
+	foreign key (id_empregado) references empregados(id_empregado)
+);
+
+select * from assalariado;
+describe assalariado;
+DELETE FROM assalariado WHERE id_empregado = 14;
+create table assalariado (
+	id_assalariado int key primary key auto_increment, 
+	salario decimal(10, 2) not null, 
+	id_empregado int not null unique, 
+	foreign key (id_empregado) references empregados(id_empregado)
+);
+
+select * from comissionado;
+describe comissionado;
+DELETE FROM comissionado WHERE id_empregado = 18;
+create table comissionado(
+	id_comissionado int key primary key auto_increment, 
+	salario decimal(10, 2) not null, 
+	taxa_venda int not null, 
+	total_vendas decimal(10, 2), 
+	id_empregado int not null unique, 
+	foreign key (id_empregado) references empregados(id_empregado)
+);
